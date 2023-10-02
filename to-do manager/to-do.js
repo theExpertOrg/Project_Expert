f6f8d8d9fc505fd3b30008f759470b2c1540211a
the Expert Org <theexpertorg23@gmail.com>
2023-10-02 13:14:08 +0530

    added html,cs and js files

---
+++ to-do manager/to-do.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosList = document.getElementById("todos");
+const todos = JSON.parse(localStorage.getItem("todos"));
