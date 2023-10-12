ba9960f9f8a4a70cda49354de170510ffb3fb299
Ajay Srivastava <ajayingithub@gmail.com>
2023-10-12 18:55:14 +0530

    login js

--- login form/login.js
+++ login form/login.js
@@ -14,3 +14,51 @@ document
       alert("Invalid username or password");
     }
   });
+
+  document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('regUsername').value;
+    const email = document.getElementById('regEmail').value;
+    const password = document.getElementById('regPassword').value;
+    const confirmPassword = document.getElementById('regConfirmPassword').value;
+  
+    // Check if all fields are filled
+    if (username && email && password && confirmPassword) {
+      // Check if passwords match
+      if (password === confirmPassword) {
+        // Successful registration
+        alert('Registration Successful');
+        // Reset the form
+        document.getElementById('registrationForm').reset();
+      } else {
+        // Passwords don't match
+        alert('Passwords do not match');
+      }
+    } else {
+      // Missing fields
+      alert('Please fill in all fields');
+    }
+  });document.getElementById('registrationForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const username = document.getElementById('regUsername').value;
+  const email = document.getElementById('regEmail').value;
+  const password = document.getElementById('regPassword').value;
+  const confirmPassword = document.getElementById('regConfirmPassword').value;
+
+  // Check if all fields are filled
+  if (username && email && password && confirmPassword) {
+    // Check if passwords match
+    if (password === confirmPassword) {
+      // Successful registration
+      alert('Registration Successful');
+      // Reset the form
+      document.getElementById('registrationForm').reset();
+    } else {
+      // Passwords don't match
+      alert('Passwords do not match');
+    }
+  } else {
+    // Missing fields
+    alert('Please fill in all fields');
+  }
+});
